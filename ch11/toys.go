1b138e5f7978befbb4133080caa7aa8f59b103fe
kenshin579 <kenshin579@hotmail.com>
2020-12-31 16:53:50 +0900

    [HFG-8] 코드 정리

--- ch11/toys.go
+++ ch11/toys.go
@@ -3,28 +3,26 @@ package main
 import "fmt"
 
 type Whistle string
+type Horn string
+type Robot string
+
+type NoiseMaker interface {
+	MakeSound()
+}
 
 func (w Whistle) MakeSound() {
 	fmt.Println("Tweet!")
 }
 
-type Horn string
-
 func (h Horn) MakeSound() {
 	fmt.Println("Honk!")
 }
-
-type Robot string
 
 func (r Robot) MakeSound() {
 	fmt.Println("Beep Boop")
 }
 func (r Robot) Walk() {
 	fmt.Println("Powering legs")
-}
-
-type NoiseMaker interface {
-	MakeSound()
 }
 
 func play(n NoiseMaker) {
@@ -40,5 +38,6 @@ func main() {
 	toy.MakeSound()
 	toy = Horn("Toyco Blaster")
 	toy.MakeSound()
+
 	play(Robot("Botco Ambler"))
 }
